60dc495d433096fedc8749d639a00992b0025e0f
Sunset <sunrisebestrise@gmail.com>
2019-07-09 14:29:39 +0200

    added map panning

--- Tilemap/main.lua
+++ Tilemap/main.lua
@@ -3,10 +3,13 @@ local sti = require "sti"
 function love.load()
 	-- Grab window size
 	windowWidth  = love.graphics.getWidth()
-	windowHeight  love.graphics.getHeight()
+	windowHeight = love.graphics.getHeight()
 
 	-- Set world meter size (in pixels)
 	love.physics.setMeter(32)
+
+	-- Prepare translations
+	tx, ty = 0, 0
 
 	-- Load a map exported to Lua from Tiled
 	map = sti("assets/test.lua", { "box2d" })
@@ -47,16 +50,28 @@ end
 
 function love.update(dt)
 	map:update(dt)
+
+	-- Move map
+	local kd = love.keyboard.isDown
+	tx = (kd("a", "left")  and tx - 128 * dt) or tx -- if button pressed then change tx, otherwise don't change tx
+	tx = (kd("d", "right") and tx + 128 * dt) or tx
+	ty = (kd("w", "up")    and ty - 128 * dt) or ty
+	ty = (kd("s", "down")  and ty + 128 * dt) or ty
 end
 
 function love.draw()
 	-- Draw the map and all objects within
 	love.graphics.setColor(1, 1, 1)
-	map:draw()
+	map:draw(-tx, -ty)
 
 	-- Draw Collision Map (useful for debugging)
 	love.graphics.setColor(1, 0, 0)
 	map:box2d_draw()
+
+	-- Translate world so that player is always centred
+	local player = map.layers["Sprite Layer"].sprites.player
+	local tx = math.floor(player.x - love.graphics.getWidth()  / 2)
+	local ty = math.floor(player.y - love.graphics.getHeight() / 2)
 
 	-- Please note that map:draw, map:box2d_draw, and map:bump_draw take
 	-- translate and scale arguments (tx, ty, sx, sy) for when you want to
