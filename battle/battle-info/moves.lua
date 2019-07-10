a8898692e80d654400ef215630fa26e1b9284483
Sunset <sunrisebestrise@gmail.com>
2019-07-10 15:29:34 +0200

    updated moves

--- battle/battle-info/moves.lua
+++ battle/battle-info/moves.lua
@@ -5,16 +5,18 @@ return {
 	scratch = {
 		id = 01,
 		name = "Scratch",
-		type = "purple",
+		magic = "purple",
 		description = "The user uses their long nails to scratch their opponent.",
+		type = "physical"
 		strength = 50,
 		properties = {},
 	},	
 	humiliate = {
 		id = 02,
 		name = "Humiliate",
-		type = "purple",
+		magic = "purple",
 		description = "The user insults their opponent until the opponent cries. The next time the opponent uses a move, the opponent will hit themselves (if the opponent uses a healing item in the next turn, the user will steal it.)",
+		type = special
 		strength = 0,
 		properties = {
 			status = status.humiliated,
@@ -23,32 +25,46 @@ return {
 	poison_powder = {
 		id = 03,
 		name = "Poison Powder",
-		type = "purple",
+		magic = "purple",
 		description = "The user throws/blows poison powder onto the target. The target loses health and gets a poison status effect.",
+		type = "special"
 		strength = 20,
 		properties = {
-			status = status.poisoned,
+			special = special.poison
 		},
 	},
 	heart_swap = {
 		id = 04,
 		name = "Heart Swap",
-		type = "purple",
+		magic = "purple",
 		description = "The user’s health is swapped with the target’s health.",
+		type = "special"
 		strength = 0,
 		properties = {
 			special = special.swap_health
 		},
 	},
-		
+	captivating_curse = {
+		id = 05,
+		name = "Captivating Curse",
+		magic = "purple",
+		description = "The target faints if the user also does.",
+		type = "special"
+		strength = 0,
+		properties = {
+			special = special
+		},
+	},	
+
 }
 --[[
 a_move = {
 		id = 01,
 		name = "Name",
-		type = "colour",
-		description = "Description",
+		magic = "colour",
+		description = "description",
+		type = "special/physical"
 		strength = 0,
 		properties = {},
 	},	
-]]--
+	]]--
--- battle/battle-info/special.lua
+++ battle/battle-info/special.lua
@@ -1,5 +1,14 @@
 return {
-	swap_health = function swap_hp(user, target) 
-		user.hp, target.hp = target.hp, user.hp
+	swap_health = function (user, target) 
+		user.hp, target.hp = target.hp, user.hp 
+	end,
+	poison = function (target)
+		--[[ 
+
+		* check of target.status_effects leeg is
+		- als het leeg is, maak target.status_effects = {"poisoned"}
+		- als index 1 vol zit, ga naar index 2
+		
+		]]--
 	end
 }
